c6eec02ef0a06e26a1201386bfd3c2140f2ab376
mkd <monkeydioude@gmail.com>
2023-08-04 18:04:03 +0200

    fixed url with query strings

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 
@@ -12,16 +12,23 @@ func main() {
 	m := moon.Moon()
 	m.AddHeader("Access-Control-Allow-Origin", "*")
 	m.MakeRouter(
-		moon.Get("/{url}", func(r *moon.Request) ([]byte, int, error) {
-			resp, err := http.Get(r.Matches["url"])
-			if err != nil {
-				return []byte("ko"), 400, nil
+		moon.Get("/bypasscors/{url}", func(r *moon.Request) ([]byte, int, error) {
+			url := r.Matches["url"]
+			if r.HTTPRequest.URL.RawQuery != "" {
+				url += "?" + r.HTTPRequest.URL.RawQuery
 			}
-			body, err := ioutil.ReadAll(resp.Body)
+			resp, err := http.Get(url)
+
 			if err != nil {
 				log.Fatalln(err)
 				return []byte("ko"), 400, nil
 			}
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatalln(err)
+				return []byte("ko"), 400, nil
+			}
+			log.Printf("OK call to %s\n", url)
 			return body, 200, nil
 		}),
 	)
